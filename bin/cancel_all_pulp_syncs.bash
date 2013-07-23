65c02a1895fc52e0c128154d86a958ada3e3027c
Mike McCune <mmccune@redhat.com>
2013-07-23 11:40:35 -0700

    fixing typo

--- bin/cancel_all_pulp_syncs.bash
+++ bin/cancel_all_pulp_syncs.bash
@@ -2,6 +2,6 @@
 
 # Need httpd stopped but mongo running
 service httpd stop
-echo "db.queued_calls.drop();" >> /tmp/queued_calls.js
+echo "db.queued_calls.drop();" >> /tmp/queued_calls_drop.js
 mongo pulp_database < /tmp/queued_calls_drop.js
 katello-service restart
